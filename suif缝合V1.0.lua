716d5cb55a41e4c3142a409abe0e8c4dd80198b9
suif666 <sfbdsl666@gmail.com>
2026-01-18 20:45:32 +0800

    Add files via upload

--- suif缝合V1.0.lua
+++ suif缝合V1.0.lua
@@ -9,6 +9,22 @@ function gradient(text, startColor, endColor)
         startColor = Color3.fromRGB(255,255,255)
         endColor = Color3.fromRGB(200,200,200)
     end
+    
+local result = ""
+    local length = #text
+
+    for i = 1, length do
+        local t = (i - 1) / math.max(length - 1, 1)
+        local r = math.floor((startColor.R + (endColor.R - startColor.R) * t) * 255)
+        local g = math.floor((startColor.G + (endColor.G - startColor.G) * t) * 255)
+        local b = math.floor((startColor.B + (endColor.B - startColor.B) * t) * 255)
+
+        local char = text:sub(i, i)
+        result = result .. string.format('<font color="rgb(%d,%d,%d)">%s</font>', r, g, b, char)
+    end
+
+    return result
+end
 
 local Tabs = {}
 
