008bcc8088bcc465ee0af78abc1e2df86ef1c43e
suif666 <sfbdsl666@gmail.com>
2026-06-21 10:57:27 +0800

    Update suif.lua

--- suif.lua
+++ suif.lua
@@ -724,7 +724,7 @@ stgTab:Button({
 
 stgTab:Button({
     Title = "ringta", Desc = "无卡密 老朋友了 更新速度还算可以", Icon = "shell",
-    Callback = function() run("", "死铁轨ringta") end
+    Callback = function() run("https://raw.githubusercontent.com/suif666/suif/refs/heads/main/Ringta%E6%AD%BB%E9%93%81%E8%BD%A8.lua", "死铁轨ringta") end
 })
 
 slTab:Button({
