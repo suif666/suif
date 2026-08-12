d341b76fbfac80cd13c4a654922fda6437dfad62
suif666 <sfbdsl666@gmail.com>
2026-08-12 15:50:58 +0800

    Update suif.lua

--- suif.lua
+++ suif.lua
@@ -633,10 +633,12 @@ fkgsTab:Button({
 })
 
 --邪恶事情远程
-getgenv().Tabs.GameTab = xesq
-getgenv().SutureGameTab = xeaq
-loadRemote("https://raw.githubusercontent.com/suif666/testing/refs/heads/main/%E9%82%AA%E6%81%B6%E4%BA%8B%E6%83%85%E7%A4%BA%E4%BE%8B.lua?t=" .. tostring(tick()), "游戏辅助")
+ getgenv().Tabs.GameTab = xesqTab
+  getgenv().SutureGameTab = xesqTab
+  loadRemote("https://raw.githubusercontent.com/suif666/testing/refs/heads/main/
+  %E9%82%AA%E6%81%B6%E4%BA%8B%E6%83%85%E7%A4%BA%E4%BE%8B.lua?t=" .. tostring(tick()), "邪恶事情")
 
+	
 wqkTab:Button({
     Title = "武器库 静默瞄准", Desc = "没有esp 但是有静默瞄准", Icon = "shell",
     Callback = function()
