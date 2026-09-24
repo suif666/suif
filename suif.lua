5c40910f60ef9035de29d640a435c4a3416300c3
suif <sfbdsl666@gmail.com>
2026-09-25 00:05:12 +0800

    suif: 库版本参数升到 expand8

--- suif.lua
+++ suif.lua
@@ -1,5 +1,5 @@
 local ok, WindUI = pcall(function()
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/suif666/suif/refs/heads/main/WindUI-Boreal.lua?v=expand7"))()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/suif666/suif/refs/heads/main/WindUI-Boreal.lua?v=expand8"))()
 end)
 
 if not ok or not WindUI then
